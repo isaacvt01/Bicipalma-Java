63aa12e0afc110ad56a79ff42dcd6074fb56b9ca
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-02-02 13:42:01 +0100

    (feat) Añadir lógica para pasar los tests creados

--- src/main/java/edu/craptocraft/bicicleta/Bicicleta.java
+++ src/main/java/edu/craptocraft/bicicleta/Bicicleta.java
@@ -1,4 +1,20 @@
 package edu.craptocraft.bicicleta;
 
 public class Bicicleta {
+
+    private int id;
+
+    public Bicicleta(int id) {
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.id);
+        return sb.toString();
+    }
 }
--- src/test/java/edu/craptocraft/BicicletaTest/BicicletaTest.java
+++ src/test/java/edu/craptocraft/BicicletaTest/BicicletaTest.java
@@ -1,11 +1,17 @@
 package edu.craptocraft.BicicletaTest;
 import org.junit.*;
-import bicicleta.*;
+import edu.craptocraft.bicicleta.*;
 
 public class BicicletaTest {
     @Test
     public void contructorTest(){
         Bicicleta bic = new Bicicleta(444);
         Assert.assertEquals(444, bic.getId());
+    }
+    @Test
+    public  void toStringTest(){
+        Bicicleta bic = new Bicicleta(888);
+        String actual = "888";
+        Assert.assertEquals(actual, bic.toString());
     }
 }
