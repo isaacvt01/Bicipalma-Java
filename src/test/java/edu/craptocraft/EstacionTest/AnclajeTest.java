b2b3c74c557f33654a095dc6d3b5b0c305643958
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-02-02 13:47:47 +0100

    (test) crear test constructor Anclaje

--- src/test/java/edu/craptocraft/EstacionTest/AnclajeTest.java
+++ src/test/java/edu/craptocraft/EstacionTest/AnclajeTest.java
@@ -1,4 +1,16 @@
 package edu.craptocraft.EstacionTest;
+import edu.craptocraft.bicicleta.Bicicleta;
+import edu.craptocraft.bicicleta.Movil;
+import edu.craptocraft.estacion.Anclaje;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class AnclajeTest {
+    @Test
+    public void constructorTest(){
+        Movil bic = new Bicicleta(444);
+        Anclaje anclaje = new Anclaje(false,bic );
+        Assert.assertEquals(444, anclaje.getBici());
+        Assert.assertEquals(false, anclaje.isOcupado());
+    }
 }
