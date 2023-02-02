8ff9a18d9ac2e3d61275470a952b5a54e1e09662
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-02-02 13:28:50 +0100

    (test) Crear test bicicletas

--- src/test/java/edu/craptocraft/BicicletaTest/BicicletaTest.java
+++ src/test/java/edu/craptocraft/BicicletaTest/BicicletaTest.java
@@ -1,4 +1,11 @@
 package edu.craptocraft.BicicletaTest;
+import org.junit.*;
+import bicicleta.*;
 
 public class BicicletaTest {
+    @Test
+    public void contructorTest(){
+        Bicicleta bic = new Bicicleta(444);
+        Assert.assertEquals(444, bic.getId());
+    }
 }
