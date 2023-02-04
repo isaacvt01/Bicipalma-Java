9816563c180c55f5e5f5cfdbe2d851e46ec4d9b4
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-02-05 00:44:44 +0100

    (test) Crear test isActivada

--- src/test/java/edu/craptocraft/TarjetaUsuarioTest/TarjetaUsuarioTest.java
+++ src/test/java/edu/craptocraft/TarjetaUsuarioTest/TarjetaUsuarioTest.java
@@ -1,4 +1,20 @@
 package edu.craptocraft.TarjetaUsuarioTest;
 
+import edu.craptocraft.tarjetausuario.TarjetaUsuario;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TarjetaUsuarioTest {
+    static TarjetaUsuario tarjeta;
+
+    @BeforeClass
+    public static void inicializar(){
+        tarjeta = new TarjetaUsuario("234", true);
+    }
+    @Test
+    public void isactivadaTest(){
+        Assert.assertTrue(tarjeta.isActivada());
+    }
 }
